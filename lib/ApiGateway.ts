a71e7d7d8f858d28a6c9d6b4740d934cd8dcd55a
jackdo69 <jackie.duc.do@gmail.com>
2022-05-30 22:37:37 +1000

    finish tutorial 2

--- lib/ApiGateway.ts
+++ lib/ApiGateway.ts
@@ -1,26 +1,42 @@
 import { RemovalPolicy } from "aws-cdk-lib";
-import { LambdaIntegration, LogGroupLogDestination, RestApi } from "aws-cdk-lib/aws-apigateway";
+import {
+  AuthorizationType,
+  CognitoUserPoolsAuthorizer,
+  LambdaIntegration,
+  LogGroupLogDestination,
+  RestApi
+} from "aws-cdk-lib/aws-apigateway";
 import { IFunction } from "aws-cdk-lib/aws-lambda";
 import { LogGroup, RetentionDays } from "aws-cdk-lib/aws-logs";
 import { Construct } from "constructs";
+import { Cognito } from "./Cognito";
 
 export class ApiGateway extends RestApi {
-    constructor(scope: Construct) {
-        const ID = "ApiGateway";
-        super(scope, ID, {
-            restApiName: 'cloud-native-app',
-            deployOptions: {
-                accessLogDestination: new LogGroupLogDestination(new LogGroup(scope, "ApiLogGroup", {
-                    logGroupName: "api_gateway",
-                    retention: RetentionDays.ONE_DAY,
-                    removalPolicy: RemovalPolicy.DESTROY
-                }))
-            }
-        })
-    }
+  private _authorizer: CognitoUserPoolsAuthorizer;
+  constructor(scope: Construct) {
+    const ID = "ApiGateway";
+    super(scope, ID, {
+      restApiName: "cloud-native-app",
+      deployOptions: {
+        accessLogDestination: new LogGroupLogDestination(
+          new LogGroup(scope, "ApiLogGroup", {
+            logGroupName: "api_gateway",
+            retention: RetentionDays.ONE_DAY,
+            removalPolicy: RemovalPolicy.DESTROY
+          })
+        )
+      }
+    });
 
-    addIntegration(method: string, path: string, lambda: IFunction) {
-        const resource = this.root.resourceForPath(path);
-        resource.addMethod(method, new LambdaIntegration(lambda))
-    }
+    const auth = new Cognito(scope);
+    this._authorizer = auth.authorizer;
+  }
+
+  addIntegration(method: string, path: string, lambda: IFunction) {
+    const resource = this.root.resourceForPath(path);
+    resource.addMethod(method, new LambdaIntegration(lambda), {
+      authorizationType: AuthorizationType.COGNITO,
+      authorizer: this._authorizer
+    });
+  }
 }
